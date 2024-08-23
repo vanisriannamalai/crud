5dba2216e7f4b7aaed956b4fdd68c681d8509124
vanisriannamalai <vanisri026@gmail.com>
2024-08-23 15:05:48 +0530

    connect to db mongodb atlas

--- controllers/movie.controllers.js
+++ controllers/movie.controllers.js
@@ -11,19 +11,17 @@ export const MovieIndex=async(req,res)=>{
 }
 
 export const MovieCreate=async(req,res)=>{
-    // const newMovie=new Movie({
-    console.log(req.body);
-    return res.json(req.body)
-    //     title:req.body.title,
-    //     desc:req.body.desc,
-    // });
-    // try{
-    //     const movie=await newMovie.save();
-    //     return res.status(201).json(movie);
-    // }
-    // catch(error){
-    //     return res.status(400).json({message:error.message});
-    // }
+    const newMovie=new Movie({
+        title:req.body.title,
+        desc:req.body.desc,
+    });
+    try{
+        const movie=await newMovie.save();
+        return res.status(201).json(movie);
+    }
+    catch(error){
+        return res.status(400).json({message:error.message});
+    }
 }
 
 export const MovieDetails=async(req,res)=>{
